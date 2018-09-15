168fbc790418d12ecf33cb1d9d8f3599510c02cd
Daniel-Cross <crossy1686@gmail.com>
2018-09-15 18:00:39 +0100

    step 3 completed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const getActivity = require('./controllers/createActivity');
 
 const app = express();
 
 app.listen(3000, () => console.log('Micro-life app listening on port 3000'));
+app.post('/profile/activities', getActivity);
