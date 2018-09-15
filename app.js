64bbcb92d81069ced9068079c4e1ffdee576c63b
Daniel-Cross <crossy1686@gmail.com>
2018-09-15 16:24:17 +0100

    initial re-commit

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const express = require('express');
-const createActivity = require('./controllers/createActivity');
-const getActivities = require('./controllers/getActivities');
 
 const app = express();
 
-app.post('/profile/activities', createActivity);
-app.get('/profile/activities', getActivities);
-
-app.listen(3000, () => console.log('App listening on port 3000'));
+app.listen(3000, () => console.log('Micro-life app listening on port 3000'));
--- controllers/getMicroLifeAdjustment.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const getMicroLifeAdjustment = (req, res) => {};
-
-module.exports = getMicroLifeAdjustment;
